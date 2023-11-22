e2a72677560933e8e9388ec44f2aa3be3858d5fb
Rasior <oliveeira.raphael@hotmail.com>
2023-11-22 18:28:59 -0300

    Update

---
+++ catálogo/catalogo.js
@@ -0,0 +1,12 @@
+var filmeUm = 'https://upload.wikimedia.org/wikipedia/pt/d/d6/Intouchables_cartaz.jpg';
+
+var filmeDois = 'https://upload.wikimedia.org/wikipedia/pt/a/a8/Up_p%C3%B4ster.jpg';
+
+var filmeTres = 'https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg';
+
+var listaFilmes = []
+listaFilmes[0] = "https://upload.wikimedia.org/wikipedia/pt/d/d6/Intouchables_cartaz.jpg";
+listaFilmes[1] = "https://upload.wikimedia.org/wikipedia/pt/a/a8/Up_p%C3%B4ster.jpg";
+listaFilmes[2] = "https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg";
+
+document.write("<img src=" + listaFilmes[0] + ">");
